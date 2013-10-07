bcd749804fac60f2408cd0b2e2ef2f437d2bb128
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-07 18:26:19 +0100

    Copied code from other DNS handler

--- spec/pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler_spec.rb
+++ spec/pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 require_relative '../../pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler'
 
 describe Wonga::Daemon::PantryDnsRecordDeleteCommandHandler do
+  let(:logger) { instance_double('Logger').as_null_object }
+  let(:publisher) { instance_double('Publisher').as_null_object }
+  subject { described_class.new(publisher, logger) }
   it_behaves_like 'handler'
+  
   describe "#handle_message" do
-    #add in unit test code for handle message here.
+    
   end
 end
 
