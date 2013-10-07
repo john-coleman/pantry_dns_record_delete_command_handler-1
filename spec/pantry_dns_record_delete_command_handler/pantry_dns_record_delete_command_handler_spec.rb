7d291a2678cc85976444e3223811e84171c4a31f
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-07 17:10:58 +0100

    first commit

---
+++ spec/pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler'
+
+describe Wonga::Daemon::PantryDnsRecordDeleteCommandHandler do
+  it_behaves_like 'handler'
+  describe "#handle_message" do
+    #add in unit test code for handle message here.
+  end
+end
+
