356c6fd284ea3819ab8d1eacc88446e67cf8cb99
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-10-09 14:51:06 +0100

    TD-1308

--- pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler.rb
+++ pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler.rb
@@ -4,32 +4,40 @@ require 'resolv'
 module Wonga
   module Daemon
     class PantryDnsRecordDeleteCommandHandler
-      def initialize(publisher, logger)
+      def initialize(publisher, logger, config)
         @publisher = publisher
         @logger = logger
+        @config = config
       end
 
       def handle_message(message)
-        ec2_instance = AWSResource.new.find_server_by_id message["instance_id"]
         hostname = message["instance_name"]
         domain = message["domain"]
-        ≈"FQDN is: #{hostname}.#{domain}")
-        
-        resolver = Resolv::DNS.new()
-        name_server = resolver.getresource(domain, Resolv::DNS::Resource::IN::NS).name
+        @logger.info("FQDN is: #{hostname}.#{domain}")
+        name_server = get_name_server(@config['daemon']['name_server'], domain)
         @logger.info("Name Server located: #{name_server}")
-        
         runner = WinRMRunner.new
-        @logger.info "WinRM Run command"
-        puts name_server
-        runner.add_host name_server
+        @logger.info("WinRM Run command")
+        @logger.info("Adding #{name_server} name server")
+        runner.add_host(name_server)
         
         # http://technet.microsoft.com/en-us/library/cc772069.aspx#BKMK_15
         # syntax: dnscmd <NameServer> /recorddelete <ZoneName> <NodeName> <RRType> <RRData> [/f]
         command = "dnscmd #{name_server} /recorddelete #{domain} #{hostname}.#{domain}. A /f"
         @logger.info(command)
-        runner.run_commands(command)
+        result = runner.run_commands(command)
+        @logger.info(result.inspect)
         @publisher.publish(message)
+      end
+      
+      def get_name_server(name_server, domain)
+        unless name_server && name_server.empty?
+          resolver = Resolv::DNS.new
+          resource = resolver.getresource(domain, Resolv::DNS::Resource::IN::NS)
+          server_name = resource.name
+        end
+        
+        name_server
       end
     end
   end
--- spec/pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler_spec.rb
+++ spec/pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler_spec.rb
@@ -4,11 +4,32 @@ require_relative '../../pantry_dns_record_delete_command_handler/pantry_dns_reco
 describe Wonga::Daemon::PantryDnsRecordDeleteCommandHandler do
   let(:logger) { instance_double('Logger').as_null_object }
   let(:publisher) { instance_double('Publisher').as_null_object }
-  subject { described_class.new(publisher, logger) }
+  let(:config) { instance_double('Wonga::Daemon.load_config', {'daemon' => {'name_server' => 'a_server'}}).as_null_object }
+  let(:win_rm_runner) { instance_double('Wonga::Daemon::WinRMRunner').as_null_object }
+  let(:message) { { "instance_name" => "hellraiser", "domain" => 'aws.example.com'} }
+  subject { described_class.new(publisher, logger, config) }
   it_behaves_like 'handler'
   
   describe "#handle_message" do
+    before(:each) do
+      Wonga::Daemon::WinRMRunner.stub(:new).and_return(win_rm_runner)
+    end
     
+    it "should receive add host" do
+      subject.handle_message(message)
+      expect(win_rm_runner).to have_received(:add_host)
+    end
+    
+    it "should receive run commands" do
+      subject.handle_message(message)
+      expect(win_rm_runner).to have_received(:run_commands)
+    end
+  end
+  
+  describe "get_name_server" do
+    it "should return a name server from config file" do
+      resolver = Resolv::DNS.stub(:new).and_return(instance_double('Resolv::DNS').as_null_object)
+      subject.get_name_server('a_server', 'aws.example.com').should == 'a_server'
+    end
   end
 end
-
