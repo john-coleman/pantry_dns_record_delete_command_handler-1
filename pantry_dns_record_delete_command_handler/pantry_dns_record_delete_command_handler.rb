deebbaefa0d5fc1e986bd22cd0ee9fe5fd482e84
Kieran Manning <kieran.manning@wonga.com>
2013-10-16 17:03:35 +0100

    updating

--- pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler.rb
+++ pantry_dns_record_delete_command_handler/pantry_dns_record_delete_command_handler.rb
@@ -4,42 +4,47 @@ require 'resolv'
 module Wonga
   module Daemon
     class PantryDnsRecordDeleteCommandHandler
-      def initialize(publisher, logger, config)
-        @publisher = publisher
-        @logger = logger
-        @config = config
+      def initialize(ad_username, ad_password, publisher, logger, config)
+        @ad_username = ad_username
+        @ad_password = ad_password
+        @publisher   = publisher
+        @logger      = logger
+        @config      = config
       end
 
       def handle_message(message)
-        fqdn = message["node"].split(".")
-        hostname = fqdn[0]
-        domain = fqdn.slice(1, fqdn.length)
-
-        @logger.info("FQDN is: #{fqdn}")
-        name_server = get_name_server(@config['daemon']['name_server'], domain)
+        fqdn         = message["node"].split(".") 
+        hostname     = fqdn[0]
+        domain       = fqdn.slice(1, fqdn.length).join(".")        
+        name_server  = get_name_server(@config['daemon']['name_server'], domain)
         @logger.info("Name Server located: #{name_server}")
-        runner = WinRMRunner.new
-        @logger.info("WinRM Run command")
-        @logger.info("Adding #{name_server} name server")
-        runner.add_host(name_server)
-        
+        delete_record(name_server, domain, hostname)
+        @logger.info("Complete. Publishing to topic")
+        @publisher.publish(message)        
+      end
+
+      def delete_record(name_server, domain, hostname) 
+        runner      = WinRMRunner.new        
+        @logger.info("WinRM Adding host: #{name_server}")
+        runner.add_host(name_server, @ad_username, @ad_password)
         # http://technet.microsoft.com/en-us/library/cc772069.aspx#BKMK_15
-        # syntax: dnscmd <NameServer> /recorddelete <ZoneName> <NodeName> <RRType> <RRData> [/f]
-        command = "dnscmd #{name_server} /recorddelete #{domain} #{hostname}.#{domain}. A /f"
-        @logger.info(command)
-        result = runner.run_commands(command)
-        @logger.info(result.inspect)
-        @publisher.publish(message)
+        # syntax: dnscmd <NameServer>   /recorddelete <ZoneName> <NodeName> <RRType> <RRData> [/f]
+        command     = "dnscmd #{name_server} /recorddelete #{domain} #{hostname}.#{domain} A /f"
+        @logger.info("WinRM exec: #{command}")
+        result      = runner.run_commands(command)
+        @logger.info("WinRM returned: #{result.inspect}")
       end
       
       def get_name_server(name_server, domain)
-        unless name_server && name_server.empty?
-          resolver = Resolv::DNS.new
-          resource = resolver.getresource(domain, Resolv::DNS::Resource::IN::NS)
-          server_name = resource.name
+        if name_server.nil? || name_server.empty?
+          resolver  = Resolv::DNS.new
+          resolver.getresource(
+            domain, 
+            Resolv::DNS::Resource::IN::NS
+          ).name
+        else
+          name_server
         end
-        
-        name_server
       end
     end
   end
